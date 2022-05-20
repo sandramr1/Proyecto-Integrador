8fbef099c2f304157879a91ab634d2bfaaba7d04
ivan9318 <61205419+ivan9318@users.noreply.github.com>
2022-05-20 17:41:49 -0500

    pruebas de validacion de datos

--- prueba.js
+++ prueba.js
@@ -1,7 +1,7 @@
 const nombre=document.getElementById('name');
 const email=document.getElementById('email');
 const form=document.getElementById('form');
-const numero=document.getElementById('numero')
+const numero=document.getElementById('numero');
 const parrafo=document.getElementById('warnings');
 form.addEventListener('submit',e=>{
     e.preventDefault();//Esta linea de codigo evita que se envie  por default,que se quede estatico
@@ -13,19 +13,30 @@ form.addEventListener('submit',e=>{
     parrafo.innerHTML='';
     if(!regexName.test(nombre.value)||nombre.value==""){
         warnings+='El nombre esta mal escrito \t';
+        
         entrar=true;
     }
     if(!regexEmail.test(email.value)||email.value==""){//en esta linea de codigo vamos a hacer un test del valor de nuestro email con nuestro regex
         warnings+='El email no es valido ';
+        
         entrar=true;
     }
     if(!regexNumber.test(numero.value)||numero.value==""){//en esta linea de codigo vamos a hacer un test del valor de nuestro email con nuestro regex
         warnings+='El numero no es valido ';
+       
         entrar=true;
     }
     if(entrar){
         parrafo.innerHTML=warnings;
     }else{
+        localStorage.nombre = document.getElementById('nombre').value;
+        localStorage.numero=document.getElementById('')
         parrafo.innerHTML='enviado';
     }
+
 });
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+    
+   
+    });
